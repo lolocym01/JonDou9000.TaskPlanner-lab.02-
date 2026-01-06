021d15552bdc57aa422e6a4c7d45a159e1b5ea14
lolocym01 <kperekorinskiji@gmail.com>
2026-01-06 23:13:20 +0200

    ABOBA

---
+++ JonDou9000.TaskPlanner.Domain.Logic/Domain.Logic.cs
@@ -0,0 +1,21 @@
+﻿using JonDou9000.TaskPlanner.Domain.Models;
+using JonDou9000.TaskPlanner.DataAccess.Abstractions;
+
+namespace JonDou9000.TaskPlanner.Domain.Logic
+{
+    public class SimpleTaskPlanner
+    {
+        private readonly IWorkItemsRepository _repository;
+
+        public SimpleTaskPlanner(IWorkItemsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public WorkItem[] CreatePlan()
+        {
+            var items = _repository.GetAll();
+            return items;
+        }
+    }
+}
